b8a6925b9674ba6384e082188dcb5fb1fd583b22
Marco Martinez <email": null>
2016-07-31 22:35:25 -0400

    add display runner and controller

--- app/controller/user_controller.rb
+++ app/controller/user_controller.rb
@@ -1,20 +1,12 @@
-# require_relative '../models/student'
-  # module UserController
-  # def self.run_program
-   p Student
-  # end
-  # def self.run_program
-  #   ClearScreen.reset_screen
-  #   UserInterface.display_start_options
-  #   user_choice = gets.chomp.to_i
-  #   ClearScreen.reset_screen
-  #   if user_choice == 1
-  #     UserController.legislators_by_state
-  #   elsif user_choice == 2
-  #     UserController.legislators_by_party
-  #   elsif user_choice == 3
-  #     UserController.single_legislator
-  #   elsif user_choice == 4
-  #     UserController.all_states_with_count
-  #   end
-  # end
+require_relative '../models/student'
+
+ module Controller
+  def self.run
+    puts <<-HOME_SCREEN
+    View the SAT information for NYC public schools. Please enter the school you'd like to search:
+    HOME_SCREEN
+    user_school = gets.chomp
+    display_school = Student.find_by(school: user_school)
+    Display.school_info(display_school)
+  end
+end
